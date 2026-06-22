a5e5c793535ccc9b23637091c638ca025487e073
Carlos Maldonado <cgmaldo@gmail.com>
2026-06-22 12:02:32 +0200

    Apply global validations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Limpiara las propiedades no esperadas
+      whitelist: true,
+      // Genera en la respuesta una mensaje que indica que alguna propiedad no se espera
+      forbidNonWhitelisted: true,
+      // Excluye los campos undefined
+      transformOptions: {
+        exposeUnsetFields: false
+      }
+    }),
+  )
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
