f00c172c47db9101f53d3f7e68d0174c33a2dc08
Carlos Maldonado <cgmaldo@gmail.com>
2026-06-23 18:57:32 +0200

    Create dependencies strategy jwt interface jwtoken and apply to app

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,14 +3,32 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt..strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
     ConfigModule,
     TypeOrmModule.forFeature([User]),
-  ]
+    // Configuración sin necesidad de un módulo asíncrono eligiendo la estrategia JWT
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET') || '',
+          signOptions: {
+            expiresIn: '2h'
+          }
+        }
+      }
+    }),
+  ],
+  exports: [TypeOrmModule, JwtStrategy, PassportModule, JwtModule]
 })
 export class AuthModule { }
