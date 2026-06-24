430bbe24f07e4272d992b370e8fd8413bccf026c
Carlos Maldonado <cgmaldo@gmail.com>
2026-06-24 18:28:06 +0200

    Create a apply decorators for auth token and roles

---
+++ src/auth/guards/user-role.guard.ts
@@ -0,0 +1,34 @@
+import { BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { User } from 'src/auth/entities/user.entity';
+
+@Injectable()
+export class UserRoleGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+  ) { }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+
+    const validRoles: string[] = this.reflector.get('roles', context.getHandler())
+
+    if (!validRoles || validRoles.length === 0) return true;
+
+    const req = context.switchToHttp().getRequest();
+    const user: User = req.user;
+
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    for (const role of user.roles) {
+      if (validRoles.includes(role)) {
+        return true;
+      };
+    }
+    throw new ForbiddenException(`User ${user.id} need a valid role: [${validRoles}]`);
+  }
+}
