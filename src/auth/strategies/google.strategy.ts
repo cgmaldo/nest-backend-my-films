109dd2227739e3f37a147cadf7c6a208fc34eeeb
Carlos Maldonado <cgmaldo@gmail.com>
2026-06-25 18:59:30 +0200

    Create strategy for google signin

---
+++ src/auth/strategies/google.strategy.ts
@@ -0,0 +1,32 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { config } from 'dotenv';
+import { Injectable } from '@nestjs/common';
+// import { ConfigService } from '@nestjs/config';
+
+config();
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+    constructor(
+        // private readonly configService: ConfigService,
+    ) {
+        super({
+            clientID: '340538535453-olf8othfgrteae2ksho4hi01ts1pt1bk.apps.googleusercontent.com',
+            clientSecret: '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',
+            callbackURL: `http://localhost:3000/auth/google`,
+            scope: ['email', 'profile'],
+        });
+    }
+    async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
+        const { name, emails, photos } = profile
+        const user = {
+            email: emails[0].value,
+            firstName: name.givenName,
+            lastName: name.familyName,
+            picture: photos[0].value,
+            accessToken
+        }
+        done(null, user);
+    }
+}
