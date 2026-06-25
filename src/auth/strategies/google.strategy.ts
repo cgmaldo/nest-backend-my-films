e084a3d7e16755acbc8f1ab3e1d1f983f908275b
Carlos Maldonado <cgmaldo@gmail.com>
2026-06-25 19:18:32 +0200

    Remove important data from comment

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -13,10 +13,6 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
         private readonly configService: ConfigService,
     ) {
         super({
-            // clientID: '340538535453-olf8othfgrteae2ksho4hi01ts1pt1bk.apps.googleusercontent.com',
-            // clientSecret: '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',
-            // callbackURL: `http://localhost:3000/auth/google`,
-            // scope: ['email', 'profile'],
             clientID: configService.get('CLIENTID'),
             clientSecret: configService.get('CLIENTSECRET'),
             callbackURL: configService.get('CALLBACKURL'),
