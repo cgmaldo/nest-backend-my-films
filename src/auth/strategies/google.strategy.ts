a2f84838d948f0b141a513fbfda07471213ec13b
Carlos Maldonado <cgmaldo@gmail.com>
2026-06-25 19:14:20 +0200

    Get data configuration for stragey from .env

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -2,6 +2,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { config } from 'dotenv';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 // import { ConfigService } from '@nestjs/config';
 
 config();
@@ -9,12 +10,16 @@ config();
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     constructor(
-        // private readonly configService: ConfigService,
+        private readonly configService: ConfigService,
     ) {
         super({
-            clientID: '340538535453-olf8othfgrteae2ksho4hi01ts1pt1bk.apps.googleusercontent.com',
-            clientSecret: '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',
-            callbackURL: `http://localhost:3000/auth/google`,
+            // clientID: '340538535453-olf8othfgrteae2ksho4hi01ts1pt1bk.apps.googleusercontent.com',
+            // clientSecret: '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',
+            // callbackURL: `http://localhost:3000/auth/google`,
+            // scope: ['email', 'profile'],
+            clientID: configService.get('CLIENTID'),
+            clientSecret: configService.get('CLIENTSECRET'),
+            callbackURL: configService.get('CALLBACKURL'),
             scope: ['email', 'profile'],
         });
     }
