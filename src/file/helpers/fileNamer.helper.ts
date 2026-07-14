2699ef84ec748cf40bc25e387bdbc07ea479a3d3
Carlos Maldonado <cgmaldo@gmail.com>
2026-07-14 19:12:00 +0200

    Rename file name of helper function for transform name of uploaded file

--- src/file/helpers/fileNamer.helper.ts
+++ src/file/helpers/fileNamer.helper.ts
@@ -1,12 +1,14 @@
-import { v7 as uuidv7 } from "uuid";
+import { User } from "src/auth/entities/user.entity";
 import { TypeFileExtension } from "../interfaces/TypeFilesExtension.interface";
 
 export const fileNamer = (req: Express.Request, file: Express.Multer.File, callback: Function) => {
+    if (!req.user) return callback(new Error('Bad request'), false);
+    const { id } = req.user as User;
     if (!file) return callback(new Error('File in empty'), false);
     const fileExtension = file.mimetype.split('/')[1];
     const validExtensions = TypeFileExtension;
     if (validExtensions.includes(fileExtension)) {
-        return callback(null, `${uuidv7()}.${fileExtension}`);
+        return callback(null, `${id}.${fileExtension}`);
     }
     callback(null, false);
 }
